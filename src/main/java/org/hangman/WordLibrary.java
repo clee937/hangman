b6e25c811c7eeee809af847b41945d1866c77bb9
Cheryl Lee <clee0937@gmail.com>
2024-04-25 22:28:01 +0100

    created WordLibrary

---
+++ src/main/java/org/hangman/WordLibrary.java
@@ -0,0 +1,21 @@
+package org.hangman;
+
+public class WordLibrary {
+
+    private String[] words = {"elephant", "wizard", "supercalifragilisticexpialidocious"};
+
+    //    private ArrayList<String> words = new ArrayList<>();
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
+    public String getRandomWord() {
+        return words[(int) Math.floor(Math.random() * words.length)];
+    }
+}
+//private static final Random RANDOM = new Random();
