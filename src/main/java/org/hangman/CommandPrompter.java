a6194f99d557ffc49b42381e84580f5c00c1fb69
Cheryl Lee <clee0937@gmail.com>
2024-04-28 13:10:57 +0100

    added CommandPrompter class

---
+++ src/main/java/org/hangman/CommandPrompter.java
@@ -0,0 +1,30 @@
+package org.hangman;
+
+import java.util.Scanner;
+
+public class CommandPrompter {
+    final private Scanner scanner = new Scanner(System.in);
+
+    public char getUsersGuess(Game game) {
+
+        System.out.println("Please guess a letter: ");
+
+        String guessedLetter = scanner.nextLine().toLowerCase();
+
+        boolean IsGuessALetter = game.checkIfGuessIsALetter(guessedLetter.charAt(0));
+
+        if (!IsGuessALetter) {
+            System.out.println("Not a valid guess.");
+            return this.getUsersGuess(game);
+        } else if (guessedLetter.length() > 1) {
+            System.out.println("Only one letter allowed. Please guess again: ");
+            return this.getUsersGuess(game);
+        }
+
+        System.out.println("You guessed: " + guessedLetter);
+        return guessedLetter.charAt(0);
+    }
+}
+
+
+
